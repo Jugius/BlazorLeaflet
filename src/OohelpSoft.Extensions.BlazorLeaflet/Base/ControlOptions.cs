ff26579a3529d70e716ac1a4fd9cfad734ba5433
Andrew Slusarev <jugius@gmail.com>
2026-07-24 15:38:59 +0300

    JsonStringEnumConverter instead of custom converter

--- src/OohelpSoft.Extensions.BlazorLeaflet/Base/ControlOptions.cs
+++ src/OohelpSoft.Extensions.BlazorLeaflet/Base/ControlOptions.cs
@@ -1,6 +1,9 @@
-﻿namespace OohelpSoft.BlazorLeaflet.Base;
+﻿using System.Text.Json.Serialization;
+
+namespace OohelpSoft.BlazorLeaflet.Base;
 
 public abstract class ControlOptions
 {
+    [JsonPropertyName("position")]
     public ControlPosition? Position { get; set; }
 }
--- src/OohelpSoft.Extensions.BlazorLeaflet/Base/ControlPosition.cs
+++ src/OohelpSoft.Extensions.BlazorLeaflet/Base/ControlPosition.cs
@@ -2,11 +2,18 @@
 
 namespace OohelpSoft.BlazorLeaflet.Base;
 
-[JsonConverter(typeof(JsonConverters.ControlPositionConverter))]
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum ControlPosition
 {
+    [JsonPropertyName("topleft")]
     TopLeft,
+
+    [JsonPropertyName("topright")]
     TopRight,
+
+    [JsonPropertyName("bottomleft")]
     BottomLeft,
+
+    [JsonPropertyName("bottomright")]
     BottomRight
 }
